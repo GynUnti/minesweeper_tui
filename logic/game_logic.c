d981d92d27cd6a1310365142ca1680d3d19244fe
GynUnti <marknguyen0905@gmail.com>
2026-05-24 00:46:53 -0400

    fix matrix error

--- logic/game_logic.c
+++ logic/game_logic.c
@@ -1,31 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int printMatrix(char**);
+int printMatrix(int n, int m, char matrix[n][m]);
 
 int main(void) {
   int n, m;
   n = 30;
   m = 30;
-  char matrix[n][m]; 
+  char matrix[n][m];
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
       matrix[i][j] = 0;
     }
   }
-  printMatrix(&matrix);
+  printMatrix(n, m, matrix);
   return 0;
 }
 
-int printMatrix(char** matrix) {
-  int n = sizeof(matrix);
-  int m = sizeof(matrix[0]);
+int printMatrix(int n, int m, char matrix[n][m]) {
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
-      printf("|%2c|", matrix[i][j]);
+      printf("|%1d", matrix[i][j]);
     }
-    printf("\n");
+    printf("|\n");
   }
   return 0;
 }
-
