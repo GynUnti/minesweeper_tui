33d28eab597f64a92587b2f4045a1f85cb48cae7
GynUnti <marknguyen0905@gmail.com>
2026-05-27 23:36:51 -0400

    square expand logic and print out visible matrix for user

--- logic/game_logic.c
+++ logic/game_logic.c
@@ -4,6 +4,7 @@
 
 int printMatrix(int n, int m, char matrix[n][m]);
 void shuffle(int array[], int arrayLength);
+void expand(int x, int y, int n, int m, char matrix[n+3][m+2], char visibleMatrix[n+2][m+2]);
 
 // MAIN FUNCTION
 int main(void) {
@@ -19,6 +20,12 @@ int main(void) {
     for (int j = 1; j <= m; j++) {
       matrix[i][j] = ' ';
     }
+  }
+  for (int i = 0; i <= m+1; i++) {
+    matrix[0][i] = matrix[n+1][i] = '0';
+  }
+  for (int i = 1; i <= n; i++) {
+    matrix[i][0] = matrix[i][m+1] = '0';
   }
   // Generate bombs position array. Based on the number of bombs user choose, we only choose the first n position to put the bomb in matrix 
   int* bombs = malloc(n*m * sizeof(int));
@@ -51,7 +58,11 @@ int main(void) {
   }
   free(bombs); // free memory, no longer use bombs[]
 
-  // Insert the number of bombs around a square (not at the edges, because we only count the inner n x m matrix)
+  /* ****************************************** *
+   * Insert the number of bombs around a square *
+   * (not at the edges, because we only count   *
+   * the inner n x m matrix)                    *
+   * ****************************************** */
   for (int i = 1; i <= n; i++) {
     for (int j = 1; j <= m; j++) {
       if (matrix[i][j] == 'x') continue;
@@ -65,12 +76,37 @@ int main(void) {
       if (matrix[i+1][j] == 'x') nearBombs++;
       if (matrix[i+1][j+1] == 'x') nearBombs++;
       if (nearBombs == 0) continue;
-      matrix[i][j] = nearBombs + 48; // '0' in ASCII is 48
+      matrix[i][j] = nearBombs + 48; /* '0' in ASCII is 48 */
     }
+  }
+
+  /* *********************************** *
+   * Create a matrix for visible to user.*
+   * This matrix will expand the visible *
+   * square depends on user's input.     *
+   * *********************************** */
+  char visibleMatrix[n+2][m+2]; 
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= m; j++) {
+      visibleMatrix[i][j] = '#';
+    }
+  }
+
+  /* ********************************** *
+   * Run loop for the user to enter x y *
+   * until they reach a bomb.           *
+   * ********************************** */
+
+  while (matrix[x][y] != 'x') {
+    expand(x,y,n,m,matrix,visibleMatrix);
+    printMatrix(n,m,visibleMatrix);
+    printf("Choose your next coordination (x y): ");
+    scanf("%d%d", &x, &y);
   }
 
   // Print of matrix (will change to the VISIBLE matrix for player, which only shows the squares that user has choosen
   printMatrix(n, m, matrix);
+  printf("You Lose!");
   return 0;
 }
 
@@ -100,4 +136,21 @@ void shuffle(int array[], int arrayLength) {
     array[i] = array[swapIndex];
     array[swapIndex] = temp;
   }
+}
+
+// EXPAND VISIBLE SQUARES FUNCTION
+void expand(int x, int y, int n, int m, char matrix[n+2][m+2], char visibleMatrix[n+2][m+2]) {
+  if (visibleMatrix[x][y] != '#') return; 
+  visibleMatrix[x][y] = matrix[x][y];
+  if (matrix[x][y] != ' ') 
+    return;
+  expand(x-1,y-1,n,m,matrix,visibleMatrix);
+  expand(x,y-1,n,m,matrix,visibleMatrix);
+  expand(x+1,y-1,n,m,matrix,visibleMatrix);
+  expand(x-1,y,n,m,matrix,visibleMatrix);
+  expand(x+1,y,n,m,matrix,visibleMatrix);
+  expand(x-1,y+1,n,m,matrix,visibleMatrix);
+  expand(x,y+1,n,m,matrix,visibleMatrix);
+  expand(x-1,y+1,n,m,matrix,visibleMatrix);
+  return;
 }
