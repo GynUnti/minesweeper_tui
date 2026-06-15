fab8816f69d5a630326aaa44404c94633c27e651
GynUnti <marknguyen0905@gmail.com>
2026-06-15 01:11:52 -0400

    ...

--- logic/headers.h
+++ logic/headers.h
@@ -3,8 +3,8 @@
 #include <time.h>
 #include <ncurses.h>
 
-#define MAX_ROW 50
-#define MAX_COL 50
+#define MAX_ROW 100
+#define MAX_COL 100
 #define STATE_ONGOING 0
 #define STATE_WON 1
 #define STATE_LOST 2
